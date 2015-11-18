59e69ca6dade186340c21374e4323513d391d68d
NinjaHubHH <nruesch@live.de>
2015-11-18 23:18:40 +0100

    Problem: unvollständiger upload (server)
    Der server läd die Dateien nicht richtig bzw. unvollständig hoch und
    somit stimmen die formatierungen nicht mehr. Nach ein paar stunden
    bugfixes habe ich leider keine Lösung gefunden.

--- formular.php
+++ formular.php
@@ -1,13 +1,42 @@
-<html>
+<?php
+if ($_GET['name'] != "" && $_GET['email'] != "" && $_GET['betreff'] != "" && $_GET['nachricht'] != "") {
+	mail ('support@blumenladen.de','Kontakt-Formular: ' $_GET['betreff'], 
+			'Kontakt-Formular wurde ausgefüllt von ' $_GET['name'] ' Email: ' $_GET['email'] ' Nachricht: \n\n' $_GET['nachricht'] );
+}
+?>
+
+
 <head>
-	<title> Das erste Formular  - Seite II</title>
+	<title> Formular - test </title>
 </head>
 
 <body>
-	<?php
-	$name = $_GET{"vorname"};
 	
-	echo "Hallo " .&name;
+	<h1> 
+	<?php
+	echo $_GET['name']
 	?>
+	</h1>
+
+	<h2> Kontakt-Formular</h2>
+		
+	<form action="formular.php">
+		<br>Vorname </br>
+		<input type = "text" name = "vorname" placeholder= "Name"> <!--name ist die ID -->
+		<input type="submit" value="Senden"><br>
+		
+		<br>
+		
+		<input type = "text" value="<?php echo $_GET['name']; ?>" name = "name" placeholder= "Name"><br>
+		<input type = "text" value="" name = "email" placeholder= "Email"><br>	
+		<input type = "text" value="" name = "betreff" placeholder= "Betreff"><br>
+		
+		 <!--textfeld wird mit textarea geöffnet und geschlossen, mit rows="10" zb kann man die Zeilen festlegen -->
+		 <!-- und mit cols="50" die breite des Textfeldes-->
+		<textarea name ="inhalt" placeholder ="Nachricht" rows="8" cols="40"  ></textarea>
+		
+		<br><input type="submit" value="Senden">
+		
+	</form>
 </body>
 </html>
